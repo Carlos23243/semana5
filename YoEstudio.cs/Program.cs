7e196017fa06ac295b32855b2c70ad6f8fe6b18e
Carlos23243 <cs.arceb@uea.edu.ec>
2026-01-08 13:49:44 -0500

    Add files via upload

---
+++ YoEstudio.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class YoEstudio
+{
+    static void Main()
+    {
+        string[] subjects = { "Matematicas", "Fisica", "Quimica", "Historia", "Lengua"};
+foreach (string subject in subjects)
+{
+    Console.WriteLine("yo estudio " + subject);
+}
+    }
+}
